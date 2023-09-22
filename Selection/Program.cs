7f9de4173d887513865c69e8fc0d00b3f0343fd2
David Jorge-Bates <jorgebd@stpaulsschool.org.uk>
2023-09-22 12:10:40 +0100

    Add project files.

---
+++ Selection/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Selection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static string FizzBuzz(int val)
+        {
+            string divisibility = "";
+            if (val % 3 == 0)
+            {
+                divisibility += "Fizz";
+            }
+            if (val % 5 == 0)
+            {
+                divisibility += "Buzz";
+            }
+
+            return divisibility;
+        }
+
+        static 
+    }
+}
