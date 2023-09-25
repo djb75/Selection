bb5d6e2c837792cb2a3c28fbd2f17dcbd6ef93bc
David Jorge-Bates <jorgebd@stpaulsschool.org.uk>
2023-09-25 09:33:04 +0100

    Up to question 4

--- Selection/Program.cs
+++ Selection/Program.cs
@@ -2,6 +2,14 @@
 {
     internal class Program
     {
+        enum AngleTypes
+        {
+            Acute,
+            Right,
+            Obtuse,
+            Straight,
+            Reflex
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -22,6 +30,58 @@
             return divisibility;
         }
 
-        static 
+        static bool vowelOrConsonant(char letter)
+        {
+            letter = char.ToLower(letter);
+            switch(letter)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    return true;    //Break; not needed because of return statement
+                default:
+                    return false;
+            }
+        }
+
+        static string threeDice()
+        {
+            Random rnd = new Random();
+            int die1 = rnd.Next(1, 6);
+            int die2 = rnd.Next(1, 6);
+            int die3 = rnd.Next(1, 6);
+            if (die1 == die2 && die2 == die3)
+            {
+                return "Three of a kind";
+            }
+            if (die1 == die2 || die2 == die3 || die1 == die3)
+            {
+                return "You have a pair!";
+            }
+            else
+            {
+                return "Sorry, you lose";
+            }
+        }
+
+        static AngleTypes angleSize(int angle)
+        {
+            if (angle < 90)
+            {
+                return AngleTypes.Acute;
+            }
+
+            if (angle == 90)
+            {
+                return AngleTypes.Right;
+            }
+
+            if (angle > 90)
+                {
+
+                }
+        }
     }
 }
